4640f35b1c86199d7afd2a133c9dd120396ba162
renata leal <renatamayraleal@gmail.com>
2022-02-09 19:47:31 -0300

    new version django

--- myteste/settings.py
+++ myteste/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'myteste.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        
+        'NAME': 'bancoteste',
+       
 
     }
 }
