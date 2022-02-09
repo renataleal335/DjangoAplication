98b41006c62e86ad266c23ce4bf11837c39756e8
renata leal <renatamayraleal@gmail.com>
2022-02-09 18:04:19 -0300

    update

--- myteste/settings.py
+++ myteste/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_on_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -124,5 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_on_heroku.settings(locals())
 
 
